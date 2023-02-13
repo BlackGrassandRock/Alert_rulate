397e5f2c76e069cff43cb823fe6671f67bacf547
BlackGrassandRock <111705390+BlackGrassandRock@users.noreply.github.com>
2023-02-13 02:25:39 +0100

    added login and password checking

--- src/tl_config.py
+++ src/tl_config.py
@@ -1,7 +1,7 @@
-TOKEN = '5534258522:AAFZta1POkWfTFIDNvTUIWvr6tglykeQKoc'
+TOKEN = ''
 TIMEZONE = 'Europe/Kiev'
 TIMEZONE_COMMON_NAME = 'Kiev'
 DB_USER = "root"
-DB_PASS = "1S92837asha."
+DB_PASS = ""
 DB_BASE = "rulate_bot"
-DEVELOPER = 't.me/Dyagilev_Olexandr'
+DEVELOPER = ''
